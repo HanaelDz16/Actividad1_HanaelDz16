73dde56e9e9a855bb075ddb123ea937d461781a3
HanaelDz16 <a390249@uach.mx>
2026-09-15 08:33:56 -0700

    Add temperature-based weather alerts

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ temperatura = st.number_input(
   "Introduce la temperatura en "C:",
 value=20
 )
+
+ if temperatura < 15:
+ st.info("El clima esta frio")
+ elif 15 <=temperatura <=25:
+ st.success("El clima esta templado")
+ else:
+ st.warning("El clima esta caluroso")
